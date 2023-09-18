9963aa0e4a32074600404c0f772309a4d16c7d83
frozenking <frozenking1240196@gmail.com>
2023-09-19 01:57:23 +0530

    marble: PixelEx-ify

--- aosp_marble.mk
+++ aosp_marble.mk
@@ -8,17 +8,27 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common AOSP stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit from marble device.
 $(call inherit-product, device/xiaomi/marble/device.mk)
+
+# Inherit available feature flags
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_CALL_RECORDING := true
 
 ## Device identifier
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := marble
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := lineage_marble
+PRODUCT_NAME := aosp_marble
+
+PEX_MAINTAINER := poponix
 
 # GMS
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
